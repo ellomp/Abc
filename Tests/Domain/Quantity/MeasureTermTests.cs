900631c0539de6f5b9fdee11189ce44591db17fc
ellomp <60709145+ellomp@users.noreply.github.com>
2020-03-30 20:18:05 +0300

    domain tests

---
+++ Tests/Domain/Quantity/MeasureTermTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Abc.Data.Quantity;
+using Abc.Domain.Common;
+using Abc.Domain.Quantity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Abc.Tests.Domain.Quantity
+{
+    [TestClass] public class MeasureTermTests : SealedClassTests<MeasureTerm, Entity<MeasureTermData>> { }
+}
