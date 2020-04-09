ef3bb7f57c05e4810ab2a1eacdcbc7287d5f406d
ellomp <60709145+ellomp@users.noreply.github.com>
2020-04-09 09:10:01 +0300

    Andmebaas korras

--- Soft/Data/ApplicationDbContext.cs
+++ Soft/Data/ApplicationDbContext.cs
@@ -1,33 +1,23 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Abc.Infra.Quantity;
 
 namespace Abc.Soft.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+            : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+            initializeTables(builder);
         }
 
-
-
-
-        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        //    : base(options)
-        //{
-        //}
-        //public DbSet<Facade.Quantity.MeasureView> Measures { get; set; }
-
-        //protected override void OnModelCreating(ModelBuilder builder)
-        //{
-        //    base.OnModelCreating(builder);
-        //    builder.Entity<MeasureView>().ToTable(nameof(Measures)); //muudab db tabelis nime "Measure"iks
-        //}
+        internal void initializeTables(ModelBuilder builder)
+        {
+            QuantityDbContext.InitializeTables(builder);
+        }
     }
 }
